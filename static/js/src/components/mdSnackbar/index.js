81997f191a9e487b98863bef981c95a926eb40fe
scnace <scbizu@gmail.com>
2017-03-08 13:57:43 +0800

    delete js src

--- static/js/src/components/mdSnackbar/index.js
+++
@@ -1,8 +0,0 @@
-import mdSnackbar from './mdSnackbar.vue';
-import mdSnackbarTheme from './mdSnackbar.theme';
-
-export default function install(Vue) {
-  Vue.component('md-snackbar', Vue.extend(mdSnackbar));
-
-  Vue.material.styles.push(mdSnackbarTheme);
-}
--- static/js/src/components/mdSpinner/index.js
+++
@@ -1,8 +0,0 @@
-import mdSpinner from './mdSpinner.vue';
-import mdSpinnerTheme from './mdSpinner.theme';
-
-export default function install(Vue) {
-  Vue.component('md-spinner', Vue.extend(mdSpinner));
-
-  Vue.material.styles.push(mdSpinnerTheme);
-}
