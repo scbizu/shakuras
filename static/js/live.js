ecbbebe2a689cced522a8da6c9b4a5f321c88449
scnace <scbizu@gmail.com>
2017-05-03 00:51:51 +0800

    add multi-line for delay time streaming

--- static/js/live.js
+++ static/js/live.js
@@ -7,6 +7,7 @@ var conn;
 var player = document.getElementById("player");
 
 
+
 var siderbar = new Vue({
   el:"#sidebar",
   data:{
@@ -65,6 +66,7 @@ var chats = new Vue({
       ldProgress: 0,
       msg:"",
       qa: "raw",
+      line:"line0",
       isplay:false,
   },
 
@@ -93,36 +95,90 @@ var chats = new Vue({
         }
         conn.send(this.msg);
         this.msg ="";
-    }
+    },
+
+    switchline: function(linenum){
+        var SURL ;
+       if (linenum == "line0"){
+         SURL = "http://localhost:8091/test?delaytime=2s"
+       }else if(linenum== "line1"){
+         SURL = "http://localhost:8091/test"
+       }
+
+
+
+       if (linenum!=""){
+
+        var vsrc = document.querySelector('video');
+            console.log(flvPlayer)
+            if (flvPlayer != null) {
+              flvPlayer.unload();
+              flvPlayer.detachMediaElement();
+              flvPlayer.destroy();
+              flvPlayer = null;
+              console.log("destroyed")
+            }
+
+       flvPlayer = flvjs.createPlayer({
+             isLive: true,
+             type: 'flv',
+             url: SURL,
+         });
+         flvPlayer.attachMediaElement(vsrc);
+         flvPlayer.load();
+         flvPlayer.play();
+       }
+       console.log("Clicked");
+    },
   },
 });
 
 
-// //fetch rsc
+//fetch rsc
+function loadflv(){
+  if (typeof flvPlayer === "undefined"){
+      if (flvjs.isSupported()) {
+          var vsrc = document.querySelector('video');
+          if (typeof flvPlayer !== "undefined") {
+          if (flvPlayer != null) {
+              flvPlayer.unload();
+              flvPlayer.detachMediaElement();
+              flvPlayer.destroy();
+              flvPlayer = null;
+            }
+          }
 
+         flvPlayer = flvjs.createPlayer({
+              isLive: true,
+              type: 'flv',
+              url: 'http://localhost:8091/test?delaytime=2s'
+          });
 
-if (flvjs.isSupported()) {
-    var vsrc = document.querySelector('video');
-    var flvPlayer = flvjs.createPlayer({
-        type: 'flv',
-        url: 'http://localhost:8091/test'
-    });
-    flvPlayer.attachMediaElement(vsrc);
-    flvPlayer.load();
-    flvPlayer.play();
+          flvPlayer.attachMediaElement(vsrc);
+          flvPlayer.load();
+          flvPlayer.play();
+      }
+    }
 }
 
-if (window["WebSocket"]) {
-    conn = new WebSocket("ws://localhost:8090/ws")
-    //conn close callback
-    conn.onclose = function(e){
-      chats.contents.push("聊天室网线被踹断啦(PД`q。=)>·。'゜")
-    }
+function loadws(){
+  if (window["WebSocket"]) {
+      conn = new WebSocket("ws://localhost:8090/ws")
+      //conn close callback
+      conn.onclose = function(e){
+        chats.contents.push("聊天室网线被踹断啦(PД`q。=)>·。'゜")
+      }
 
-    conn.onmessage = function(e){
-      chats.contents.push(e.data)
-    }
-}else{
-  //do not support WebSocket
-  console.log("do not support WebSocket")
+      conn.onmessage = function(e){
+        chats.contents.push(e.data)
+      }
+  }else{
+    //do not support WebSocket
+    console.log("do not support WebSocket")
+  }
 }
+
+document.addEventListener('DOMContentLoaded',function(){
+   loadflv();
+   loadws();
+})
